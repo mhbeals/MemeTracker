9f13651cb2a7fd85efb82672498b2a96e9c1abcb
M. H. Beals <m.h.beals@lboro.ac.uk>
2016-10-16 11:00:20 +0100

    Create memetrackerscriptandbatch.py

---
+++ memetrackerscriptandbatch.py
@@ -0,0 +1,88 @@
+# This script creates individual script files for running formatted match reports through the memetracker and a batch file for running all the scripts in sequence
+
+start_year = 1800
+end_year = 1900
+counter = 1
+countery = 0
+year = 0
+
+# While we are before the final year
+while year < end_year:
+    
+    # Set to new year
+    year = start_year + countery
+    
+    # While we are within a single year
+    while  counter <= 12:
+        
+        monthstring = "0" + str(counter)
+        date = str(year) + "_" + str(monthstring[-2:])
+
+        # Start file text
+        
+        string = "C:\\Documents\\Notebooks\\"
+        string += date
+        string += ".tsv"
+        string += "\n"
+        string += "D:\\Users\\M. H. Beals\\Documents\\GitHub\\MemeTracker\\Release\\"
+        string += date
+        string += ".tsv"
+        
+        # Write script file
+        filename = "script_" + date + ".txt"
+        text_file = open(filename, "w")
+        text_file.write(string)
+        text_file.close()
+        
+        # Move onto next month counter
+        counter += 1
+        
+        # Continue month loop
+        
+    # Reset month counter  
+    counter = 1
+    
+    # Update year counter
+    countery += 1
+    
+    # Continue year loop 
+
+counter = 1
+countery = 0
+year = 0
+string = ""
+
+# While we are before the final year
+while year < end_year:
+    
+    # Set to new year
+    year = start_year + countery
+    
+    # While we are within a single year
+    while  counter <= 12:
+        
+        monthstring = "0" + str(counter)
+        date = str(year) + "_" + str(monthstring[-2:])
+
+        # Start file text
+        
+        string += "MemeTracker_v_1_1.exe < script_" + date + ".txt\n"
+            
+        # Move onto next month counter
+        counter += 1
+        
+        # Continue month loop
+        
+    # Reset month counter  
+    counter = 1
+    
+    # Update year counter
+    countery += 1
+    
+    # Continue year loop 
+
+# Write script file
+filename = "memetracker".bat"
+text_file = open(filename, "w")
+text_file.write(string)
+text_file.close()
